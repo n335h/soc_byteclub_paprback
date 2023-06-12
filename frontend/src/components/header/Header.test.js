f0813aff6bf006e4e96783fda43f7acc248747b3
lozzolloz <124617157+lozzolloz@users.noreply.github.com>
2023-06-12 13:39:22 +0100

    made broken test in header to check ci/cd pipeline working

---
+++ frontend/src/components/header/Header.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Header from "./Header";
+
+describe("Header", () => {
+  test("should render logo container with correct src attribute", () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+
+    const logoElement = screen.getByAltText("Logo");
+    const srcAttribute = logoElement.getAttribute("src");
+    expect(srcAttribute).toBe("paperback_logo.svg");
+  });
+});
