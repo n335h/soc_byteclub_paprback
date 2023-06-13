664c7e9d10ece11bd9d18392d662cd736240d938
Ben Hensor <benhensor@gmail.com>
2023-06-13 12:15:19 +0100

    simple server setup and communicating with the database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,53 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import pkg from 'pg';
+const { Client } = pkg;
+dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000; // default port to listen
+const port = process.env.PORT || 5432; // default port to listen
+
+const dbConnectionString = process.env.DB_CONNECTION_STRING;
+const apiKey = process.env.API_KEY;
+
+
+
+// middleware
+app.use(express.json());
+
+
+
+async function getBooksFromDatabase() {
+  const client = new Client({
+    connectionString: dbConnectionString, // Use 'connectionString' instead of 'dbConnectionString'
+  });
+
+  try {
+    await client.connect();
+
+    const result = await client.query('SELECT * FROM books');
+    console.log(result.rows);
+  } catch (error) {
+    console.error('Error executing query:', error);
+  } finally {
+    await client.end();
+  }
+}
+
+getBooksFromDatabase();
+
+
+// route
+
+
+// app.get('/api/books', (req, res) => {
+//   // Retrieve books from the database and send a response
+//   res.send('List of books');
+// });
+
+
+
+
 
 app.listen(port, () => {
   console.log(`server started at http://localhost:${port}`);
