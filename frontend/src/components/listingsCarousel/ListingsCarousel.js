1e98254afecfe122419f2a3b7882b334a6479370
NLMpofu <124804099+NLMpofu@users.noreply.github.com>
2023-06-19 14:02:39 +0100

    Installation of carousel

--- frontend/src/components/listingsCarousel/ListingsCarousel.js
+++ frontend/src/components/listingsCarousel/ListingsCarousel.js
@@ -1,9 +1,32 @@
 import "./listingsCarousel.css";
 import Book from "../book/Book";
 import { useState, useEffect } from "react";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 
 function ListingsCarousel() {
   const [listings, setListings] = useState([]);
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
   // useEffect to fetch listings data from the API on component mount
   useEffect(() => {
@@ -27,9 +50,26 @@ function ListingsCarousel() {
   }, [listings]);
 
   return (
-    <div id="listings-carousel">
       <div className="books-grid">
-        {listings.map((book) => (
+      <Carousel
+          swipeable={false}
+          draggable={false}
+          showDots={true}
+          responsive={responsive}
+          ssr={true} // means to render carousel on server-side.
+          infinite={true}
+          autoPlay={this.props.deviceType !== "mobile" ? true : false}
+          autoPlaySpeed={1000}
+          keyBoardControl={true}
+          customTransition="all .5"
+          transitionDuration={500}
+          containerClass="carousel-container"
+          removeArrowOnDeviceType={["tablet", "mobile"]}
+          deviceType={this.props.deviceType}
+          dotListClass="custom-dot-list-style"
+          itemClass="carousel-item-padding-40-px"
+        >
+      {listings.map((book) => (
           // Rendering Book component for each listing
           <Book
             cover={book.cover_img}
@@ -37,8 +77,9 @@ function ListingsCarousel() {
             author={book.author}
           />
         ))}
+         </Carousel>
       </div>
-    </div>
+  
   );
 }
 
