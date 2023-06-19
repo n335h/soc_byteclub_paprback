62242fcd43af6c1aa7ddb155cfef45a0289303d9
Ben Hensor <benhensor@gmail.com>
2023-06-19 13:35:15 +0100

    listing fix maybe

--- frontend/src/components/listYourBook/ListYourBook.js
+++ frontend/src/components/listYourBook/ListYourBook.js
@@ -1,28 +1,41 @@
 import ListYourBookInput from '../listYourBookInput/ListYourBookInput';
 import ListYourBookOutput from '../listYourBookOutput/ListYourBookOutput';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './listYourBook.css';
 
 function ListYourBook() {
   // State variables
   const [condition, setCondition] = useState('');
   const [notes, setNotes] = useState('');
+  
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResult, setSearchResult] = useState({
+    title: '',
+    author: '',
+    cover_img: '',
+    isbn: ''
+  });
   const [newListing, setNewListing] = useState({
     title: '',
     author: '',
     isbn: '',
     condition: '',
     notes: '',
-    cover_img: '',
-    user_id: 1,
+    cover_img: ''
+    // user_id: '',
   });
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResult, setSearchResult] = useState({
-    title: '',
-    author: '',
-    cover_img: '',
-    publishedDate: '',
-  });
+
+  useEffect(() => {
+    setNewListing((prevState) => ({
+      ...prevState,
+      condition: condition,
+      notes: notes
+    }));
+  }, [condition, notes]);
+
+  useEffect(() => {
+    console.log(searchResult)
+    }, [searchResult]);
 
   // Function to handle changes in the search bar input
   function handleChange(e) {
@@ -39,31 +52,32 @@ function ListYourBook() {
   // Function to handle the search button click
   function handleSearchClick() {
     if (searchTerm) {
-      // Fetch data from the Google Books API based on the search term
       fetch(`http://localhost:5432/api/books/${searchTerm}`)
         .then((res) => {
           if (res.ok) {
             return res.json();
           } else {
-            throw new Error('No book found'); // Throw an error if no book is found
-            
+            throw new Error('No book found');
           }
+        })
+        .then((data) => {
+          setSearchResult(data.payload[0]);
           
         })
-        .then((data) => setSearchResult(data.payload[0])) // Update the searchResult state with the data from the API
         .catch((error) => {
-          console.error('Error fetching data:', error); // Log the error
+          console.error('Error fetching data:', error);
           setSearchResult({
             title: '',
             author: '',
             cover_img: '',
-            publishedDate: '',
+            isbn: '',
           });
-          alert('No book found. Please try again.'); // Display an alert
+          alert('No book found. Please try again.');
         });
-      console.log(searchResult);
     }
   }
+  
+  
 
   // Function to update the condition state
   function updateCondition(e) { // e is the event object
@@ -77,40 +91,92 @@ function ListYourBook() {
 
   // Function to handle the listing button click
   function handleListingClick() {
-    // Send a POST request to the backend API to create a new listing
-    fetch('http://localhost:5432/api/listings', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newListing),
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          throw new Error('Failed to create listing');
-        }
-      })
-      .then((data) => console.log(data))
-      .catch((error) => {
-        console.error('Error creating listing:', error);
-        // If there is an error, update the newListing object with the search result data and other values
-        setNewListing({
-          title: searchResult.title,
-          author: searchResult.author,
-          isbn: searchResult.isbn,
-          condition: condition,
-          notes: notes,
-          cover_img: searchResult.cover_img,
-          user_id: 2,
-        });
-      });
-
-    console.table(newListing);
-    console.log('List Post Clicked');
-    console.table(newListing);
+    setNewListing({
+      ...searchResult,
+      condition: condition,
+      notes: notes
+    });
   }
+
+  // Make the API call when newListing state changes
+  useEffect(() => {
+    if (newListing.title !== '') {
+      fetch('http://localhost:5432/api/listings', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newListing),
+      })
+        .then((res) => {
+          if (res.ok) {
+            console.log('Listing created successfully!');
+            return res.json();
+          } else {
+            throw new Error('Failed to create listing');
+          }
+        })
+        .then((data) => {
+          console.table(newListing);
+        })
+        .catch((error) => {
+          console.error('Error creating listing:', error);
+        });
+    }
+  }, [newListing]);
+
+  
+
+  // // Function to handle the listing button click
+  // function handleListingClick() {
+  //   // If there is an error, update the newListing object with the search result data and other values
+  //   setNewListing({
+  //     ...searchResult,
+  //     condition: condition,
+  //     notes: notes
+  //     // cover_img: searchResult.cover_img,
+  //     // user_id: 2,
+  //   });
+  //   // Send a POST request to the backend API to create a new listing
+  //   fetch('http://localhost:5432/api/listings', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(newListing),
+  //     // Include the OVERRIDING SYSTEM VALUE clause in the fetch request
+    
+  //   })
+  //     .then((res) => {
+  //       if (res.ok) {
+  //         console.log("This worked!");
+  //         // setSearchResult({
+  //         //   title: '',
+  //         //   author: '',
+  //         //   cover_img: '',
+  //         //   isbn: '',
+  //         // });
+  //         // setCondition('');
+  //         // setNotes('');
+  //         return res.json();
+          
+  //       } else {
+  //         throw new Error('Failed to create listing');
+  //       }
+  //     })
+  //     .then((data) => {
+  //       // console.log(data)
+  //       console.table(newListing);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error creating listing:', error);
+        
+  //     });
+
+  //   console.table(newListing);
+  //   console.log('List Post Clicked');
+    
+  // }
 
   // Render the component
   return (
