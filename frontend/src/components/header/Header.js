298f7c09928fc692f19121cdd4ea2451ce4ccaac
Nich H <nmjhorishny@gmail.com>
2023-06-21 11:33:30 +0100

    Auth0 Works

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -3,7 +3,8 @@ import logo from '../../assets/icons/paprback_logo.svg';
 import './header.css';
 import React, { useState, useEffect } from 'react';
 import userIconGrey from '../../assets/icons/userIconGrey.png';
-
+import LoginButton from '../loginButton/loginButton';
+import LogoutButton from '../logoutButton/logoutButton';
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -38,12 +39,11 @@ function Header() {
           </div>
           <div className="user-access">
             {/* Render signup and login buttons and link them to their respective pages */}
-            <Link to="/signup">
+            <Link to="https://dev-4lkat7o8wi7ta0m4.us.auth0.com/u/signup?state=hKFo2SBVZi1HMFpjRjRsalMwbng4ZUNILS0zeFBSS1VZODNMUKFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIFgtQ2x6TXVNanJFaXF0OWk5ZUNzUXowTDVKeUIwVzNSo2NpZNkgNENXNlRRSEYxQVlnUGlDem9GTmY2d3V2aVlPUDRMVTg">
               <button className="signup">Sign up</button>
             </Link>
-            <Link to="/login">
-              <button className="login">Login</button>
-            </Link>
+            <LoginButton />
+            <LogoutButton />
           </div>
         </div>
         <div className="menuBar">
