14665bf5ce68fddad3e7c84721c9e850d6e466d8
lozzolloz <124617157+lozzolloz@users.noreply.github.com>
2023-06-22 16:31:57 +0100

    working on back end tests

--- backend/routes.test.js
+++ backend/routes.test.js
@@ -1,87 +1,67 @@
 //import supertest from 'supertest';
-import app from "../backend/server.js";
-import Routes from "../backend/routes.js";
-//import {describe, expect, test} from '@jest/globals';
-import pool from '../backend/server.js';
-//import { Pool } from '..backend/server.js'
-const request = require('supertest')
-const express = require('express')
-//const Routes = require('../backend/routes.js')
+import Routes from "./routes.js";
+import app from "./server.js";
+import pool from "./server.js";
 
-// const pool = {};
-// function Routes(app, pool) {
-//   app.get("/", (req, res) => {
-//     res.json({
-//       status: true,
-//       payload: "This route works!",
-//     });
-//   });
-// }
+Routes(app, pool);
+const request = require("supertest");
+const express = require("express");
 
-//Routes(app, pool);
-
-
-describe('GET /', () => {
-  test('should respond with a working route', async () => {
-    const response = await request(app).get('/');
+describe("GET /", () => {
+  test("should respond with a working route", async () => {
+    const response = await request(app).get("/");
     expect(response.status).toBe(200); // Check if the response status is 200 (OK)
     expect(response.body).toEqual({
       status: true,
-      payload: "This route works!", 
+      payload: "This route works!",
     });
   });
 });
 
-describe('GET /api/books/Dune', () => {
-  test('Return json response', async () => {
-    //const mockResult = { rows: [{ isbn: '1234567890', title: 'Test Title' }] };
-    //pool.query = jest.fn().mockResolvedValue(mockResult);
-    const response = await request(app).get('/api/books/Dune');
-    //console.log(response)
+describe("GET /api/books/Dune", () => {
+  test("Return json response", async () => {
+    const response = await request(app).get("/api/books/Dune");
+
     expect(response.status).toBe(200); // Check if the response status is 200 (OK)
     expect(response.body).toEqual({
       success: true,
       payload: [
         {
-          "book_id": 14,
-          "title": "Dune",
-          "author": "Frank Herbert",
-          "isbn": "9780441172719",
-          "cover_img": "/books/dune.png"
-        }
+          book_id: 14,
+          title: "Dune",
+          author: "Frank Herbert",
+          isbn: "9780441172719",
+          cover_img: "/books/dune.png",
+        },
       ],
     });
   });
 });
 
+// describe("POST /api/listings", () => {
+//   test("Return json response", async () => {
+//     const response = await request(app).post("/api/listings").send({
+//       user_id: 1,
+//       title: "Test title",
+//       author: "Test author",
+//       isbn: "9780441172719",
+//       cover_img: "/books/test_img.png",
+//       condition: "Good",
+//       notes: "Test notes",
+//     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// describe('GET /', (req, res) => {
-//     test('should respond with a working route', async () => {
-//       const response = await app.get('/');
-//       expect(response.status).toBe(200); // Check if the response status is 200 (OK)
-//       expect(response.body).toEqual({
-//         status: true,
-//         payload: "This route works!",
-//       });
+//     expect(response.status).toBe(200); // Check if the response status is 200 (OK)
+//     expect(response.body).toEqual({
+//       success: true,
+//       payload: [{
+//         user_id: 1,
+//         title: "Test title",
+//         author: "Test author",
+//         isbn: "9780441172719",
+//         cover_img: "/books/test_img.png",
+//         condition: "Good",
+//         notes: "Test notes",
+//       }],
 //     });
 //   });
-  
+// });
