04338863b3b36e6b61757535a7ec3d2eed67e7a8
Ben Hensor <benhensor@gmail.com>
2023-06-23 14:24:53 +0100

    browse books styles tweaked

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -5,6 +5,9 @@ import React, { useState, useEffect } from 'react';
 import userIconGrey from '../../assets/icons/userIconGrey.png';
 import LoginButton from '../loginButton/loginButton';
 import LogoutButton from '../logoutButton/logoutButton';
+
+
+
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
@@ -42,8 +45,8 @@ function Header() {
             <Link to="https://dev-4lkat7o8wi7ta0m4.us.auth0.com/u/signup?state=hKFo2SBVZi1HMFpjRjRsalMwbng4ZUNILS0zeFBSS1VZODNMUKFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIFgtQ2x6TXVNanJFaXF0OWk5ZUNzUXowTDVKeUIwVzNSo2NpZNkgNENXNlRRSEYxQVlnUGlDem9GTmY2d3V2aVlPUDRMVTg">
               <button className="signup">Sign up</button>
             </Link>
-            <LoginButton />
-            <LogoutButton />
+            <LoginButton id="header-login"/>
+            <LogoutButton id="header-logout"/>
           </div>
         </div>
         <div className="menuBar">
