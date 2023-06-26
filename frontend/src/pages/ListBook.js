e1aab805cf066d62a059134b71162ce33ead31e2
Evangeline Amoura <evangelineamoura@yahoo.com>
2023-06-26 14:12:11 +0100

    positioning button and flex

--- frontend/src/pages/ListBook.js
+++ frontend/src/pages/ListBook.js
@@ -1,6 +1,7 @@
 import ListYourBook from "../components/listYourBook/ListYourBook";
 import ListingsCarousel from "../components/listingsCarousel/ListingsCarousel";
 import "./listbook.css";
+import { Link } from 'react-router-dom';
 
 
 
@@ -12,7 +13,8 @@ function ListBook() {
         <div className="list-your-book">
           <ListYourBook />
         </div>
-        <div className="User-info-container">
+        <div className= "overall-box"> 
+        <div className="user-info-container">
           <h1>Welcome Zendaya!</h1>
           <h2>CONTACT INFORMATION</h2>
           <h2>Name: Zendaya Coleman</h2>
@@ -22,11 +24,18 @@ function ListBook() {
           Coventry
           CV5 6NF</h2>
           <h2>Phone: 0121 987 654</h2>
+          <Link to="http://localhost:3000/browse">
+          <button id="find-a-book-button">
+            Find A Book
+          </button>
+        </Link>
           <img className="User-image" src="https://i.pinimg.com/474x/2b/a9/ec/2ba9ecf8e084d6b699ea6b5dd7e5575d.jpg" alt="Picture of Zendaya"></img>
         </div>
         <div className="listings-carousel-container">
           <ListingsCarousel />
         </div>
+        </div>
+
   
       </div>
       
