1b5282f282c79d17c8207c8f27a24f8f64c0c999
NLMpofu <124804099+NLMpofu@users.noreply.github.com>
2023-06-26 10:40:28 +0100

    Addition of user info and mild css

--- frontend/src/pages/ListBook.js
+++ frontend/src/pages/ListBook.js
@@ -15,6 +15,18 @@ function ListBook() {
         <div className="listings-carousel-container">
           <ListingsCarousel />
         </div>
+        <div className="User-info-container">
+          <h1>Welcome Zendaya!</h1>
+          <h2>CONTACT INFORMATION</h2>
+          <h2>Name: Zendaya Coleman</h2>
+          <h2>Email: Zcoleman@gmail.com</h2>
+          <h2>Address:
+          201 Albany Road
+          Coventry
+          CV5 6NF</h2>
+          <h2>Phone: 0121 987 654</h2>
+          <img className="User-image" src="https://i.pinimg.com/474x/2b/a9/ec/2ba9ecf8e084d6b699ea6b5dd7e5575d.jpg" alt="Picture of Zendaya"></img>
+        </div>
       </div>
       
     </section>
