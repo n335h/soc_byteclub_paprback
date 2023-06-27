462eb3d059424a1c268a5805829a624ae591b383
Nich H <nmjhorishny@gmail.com>
2023-06-27 14:31:07 +0100

    Fixed about img

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,4 +1,4 @@
-import aboutt from './../assets/images/aboutt.jpg';
+import imgabout from './../assets/images/imgabout.jpg';
 import './about.css';
 
 function About() {
@@ -7,7 +7,7 @@ function About() {
       <div className="about-container">
         <h1 id="pagetitle">About Us</h1>
         <div className="about-info">
-          <img className="aboutimg" src={aboutt} alt="about us" />
+          <img className="aboutimg" src={imgabout} alt="about us" />
           <section className="about-text">
             <p>
               <br></br>
